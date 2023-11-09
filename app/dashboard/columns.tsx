25e786f74adf0d36114f808d02e7572f2b5ce9a3
@chiennguyenduc <chiennguyenduc@channelvn.net>
2023-11-09 11:30:37 +0700

    update

--- app/dashboard/columns.tsx
+++ app/dashboard/columns.tsx
@@ -1,81 +1,111 @@
-'use client'
+"use client";
 
-import { ColumnDef } from '@tanstack/react-table'
+import { ColumnDef } from "@tanstack/react-table";
 
-import { MoreHorizontal, ArrowUpDown } from 'lucide-react'
+import { MoreHorizontal, ArrowUpDown } from "lucide-react";
+import { Checkbox } from "@/components/ui/checkbox";
 
-import { Button } from '@/components/ui/button'
+import { Button } from "@/components/ui/button";
 import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
   DropdownMenuLabel,
   DropdownMenuSeparator,
-  DropdownMenuTrigger
-} from '@/components/ui/dropdown-menu'
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
-export type User = {
-  id: string
-  name: string
-  emaiL: string
-  image: string
-  lastSeen: string
-}
+export type Product = {
+  id?: string | "";
+  name?: string | "";
+  thumb?: string | "";
+  price?: number | 0;
+};
 
-export const columns: ColumnDef<User>[] = [
+export const columns: ColumnDef<Product>[] = [
   {
-    accessorKey: 'name',
-    header: ({ column }) => {
-      return (
-        <Button
-          variant='ghost'
-          onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
-        >
-          Name
-          <ArrowUpDown className='ml-2 h-4 w-4' />
-        </Button>
-      )
-    }
+    id: "select",
+    header: ({ table }: any) => (
+      <Checkbox
+        checked={table.getIsAllPageRowsSelected()}
+        onCheckedChange={(value: any) =>
+          table.toggleAllPageRowsSelected(!!value)
+        }
+        aria-label="Select all"
+      />
+    ),
+    cell: ({ row }: any) => (
+      <Checkbox
+        checked={row.getIsSelected()}
+        onCheckedChange={(value) => row.toggleSelected(!!value)}
+        aria-label="Select row"
+      />
+    ),
+    enableSorting: false,
+    enableHiding: false,
   },
   {
-    accessorKey: 'email',
-    header: 'Email'
+    accessorKey: "name",
+    header: "Tên sản phẩm",
+    cell: ({ row }: any) => (
+      <div className="capitalize">{row.getValue("name")}</div>
+    ),
   },
   {
-    accessorKey: 'lastSeen',
-    header: 'Last seen',
-    cell: ({ row }) => {
-      const date = new Date(row.getValue('lastSeen'))
-      const formatted = date.toLocaleDateString()
-      return <div className='font-medium'>{formatted}</div>
-    }
+    accessorKey: "thumb",
+    header: "Ảnh",
+    cell: ({ row }: any) => (
+      <img src={row.getValue("thumb")} alt="image" width={100} />
+    ),
   },
   {
-    id: 'actions',
-    cell: ({ row }) => {
-      const user = row.original
+    accessorKey: "startedDate",
+    header: "Ngày nhập",
+    cell: ({ row }: any) => (
+      <div className="capitalize">{row.getValue("startedDate")}</div>
+    ),
+  },
+
+  {
+    accessorKey: "price",
+    header: () => <div className="text-right">Giá</div>,
+    cell: ({ row }: any) => {
+      const price = parseFloat(row.getValue("price"));
+      const formatted = new Intl.NumberFormat("en-US", {
+        style: "currency",
+        currency: "VND",
+      }).format(price);
+
+      return <div className="text-right font-medium">{formatted}</div>;
+    },
+  },
+  {
+    id: "actions",
+    enableHiding: false,
+    cell: ({ row }: any) => {
+      const payment = row.original;
 
       return (
         <DropdownMenu>
           <DropdownMenuTrigger asChild>
-            <Button variant='ghost' className='h-8 w-8 p-0'>
-              <span className='sr-only'>Open menu</span>
-              <MoreHorizontal className='h-4 w-4' />
+            <Button variant="ghost" className="h-8 w-8 p-0">
+              <span className="sr-only">Open menu</span>
+              <MoreHorizontal className="h-4 w-4" />
             </Button>
           </DropdownMenuTrigger>
-          <DropdownMenuContent align='end'>
-            <DropdownMenuLabel>Actions</DropdownMenuLabel>
+          <DropdownMenuContent align="end">
+            <DropdownMenuLabel>Tùy chỉnh</DropdownMenuLabel>
             <DropdownMenuItem
-              onClick={() => navigator.clipboard.writeText(user.id)}
+              onClick={() => navigator.clipboard.writeText(payment.id)}
             >
-              Copy user ID
+              Chi tiết
             </DropdownMenuItem>
             <DropdownMenuSeparator />
-            <DropdownMenuItem>View customer</DropdownMenuItem>
-            <DropdownMenuItem>View payment details</DropdownMenuItem>
+            <DropdownMenuItem>Sửa</DropdownMenuItem>
+            <DropdownMenuItem>Xóa</DropdownMenuItem>
           </DropdownMenuContent>
         </DropdownMenu>
-      )
-    }
-  }
-]
+      );
+    },
+  },
+];
--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,23 +1,21 @@
-import { User, columns } from './columns'
-import { DataTable } from '@/components/data-table'
+import { products } from "@/common/data";
+import { Product, columns } from "./columns";
+import { DataTable } from "@/components/data-table";
 
-async function getUsers(): Promise<User[]> {
-  const res = await fetch(
-    'https://64a6f5fc096b3f0fcc80e3fa.mockapi.io/api/users'
-  )
-  const data = await res.json()
-  return data
+async function getUsers(): Promise<Product[]> {
+  const data = products;
+  return data;
 }
 
 export default async function Page() {
-  const data = await getUsers()
+  const data = await getUsers();
 
   return (
-    <section className='py-24'>
-      <div className='container'>
-        <h1 className='mb-6 text-3xl font-bold'>All Users</h1>
+    <section className="py-24">
+      <div className="container">
+        <h1 className="mb-6 text-3xl font-bold">Product Manager</h1>
         <DataTable columns={columns} data={data} />
       </div>
     </section>
-  )
+  );
 }
